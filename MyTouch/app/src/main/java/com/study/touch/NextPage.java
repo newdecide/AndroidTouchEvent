8cb5c068ba2d8719d1ece8bc51b91b9ad9f91919
newdecide <liberty-1@nate.com>
2020-01-08 22:22:19 +0900

    [생성] 다음페이지 화면 클릭 이벤트 구현

---
+++ MyTouch/app/src/main/java/com/study/touch/NextPage.java
@@ -0,0 +1,38 @@
+package com.study.touch;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NextPage extends AppCompatActivity {
+
+    ImageView androidimage;
+    LinearLayout moveimage;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nextpage);
+
+        androidimage = findViewById(R.id.androidimage);
+        moveimage = findViewById(R.id.moveimage);
+
+        moveimage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        Toast.makeText(getApplicationContext(),"클릭 됐습니다.",Toast.LENGTH_SHORT).show();
+                        androidimage.setX(event.getX()-120);
+                        androidimage.setY(event.getY()-120);
+                }
+                return true;
+            }
+        });
+    }
+}
